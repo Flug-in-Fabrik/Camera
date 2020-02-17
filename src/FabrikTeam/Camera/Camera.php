7696f2ef6b08d9128a1e22e964e957c39d4cc2fb
오르카 <44334220+TrueToneTeam@users.noreply.github.com>
2020-02-17 11:54:10 +0900

    API 4.0.0

--- src/FabrikTeam/Camera/Camera.php
+++ src/FabrikTeam/Camera/Camera.php
@@ -2,26 +2,17 @@
 
 namespace FabrikTeam\Camera;
 
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\Server;
 use pocketmine\plugin\PluginBase;
-
-use pocketmine\event\Listener;
-use pocketmine\event\entity\EntityDamageEvent;
-use pocketmine\event\server\DataPacketReceiveEvent;
-use pocketmine\event\player\{PlayerMoveEvent, PlayerQuitEvent, PlayerJoinEvent, PlayerRespawnEvent, PlayerDropItemEvent, PlayerInteractEvent, PlayerChatEvent, cheat\PlayerIllegalMoveEvent};
-use pocketmine\event\block\{BlockBreakEvent, BlockPlaceEvent};
-
 use pocketmine\block\{Block, BlockFactory};
 use pocketmine\item\{Item, ItemFactory, ItemBlock, enchantment\Enchantment, enchantment\EnchantmentInstance};
-
-use pocketmine\inventory\{ShapedRecipe, ShapelessRecipe};
-
 use pocketmine\utils\Config;
 
 use FabrikTeam\Camera\Custom\Item\{CameraItem};
 
 class Camera extends PluginBase implements Listener{
+	
 	private static $instance = null;
 	
 	public static function getInstance(): Camera{
@@ -32,7 +23,7 @@ class Camera extends PluginBase implements Listener{
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		
 		/* 아이템 추가 */
-		ItemFactory::registerItem(new CameraItem(), true);
+		ItemFactory::register(new CameraItem(), true);
 		/* 아이템 추가 */
 	}
 }
